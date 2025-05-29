48b34b2aa4d22e5f5f93ec8f83f37d5d6af06916
Motrni <motrni@yandex.ru>
2025-05-29 22:57:27 +0700

    test ready

---
+++ test/test_site.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.firefox.webdriver import Options
+import time
+from pages.homepage import HomePage
+from pages.product import ProductPage
+
+
+def test_open_s6(browser):
+
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.clicl_galaxy_s6()
+    product_page = ProductPage(browser)
+    product_page.check_title_is('Samsung galaxy s6')
+
+
+def test_two_monitors(browser):
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_monitor()
+    #browser.get('https://demoblaze.com/')
+    #monitor_link = browser.find_element(By.CSS_SELECTOR, value = '''[onclick="byCat('monitor')"]''')
+    #monitor_link.click()
+    time.sleep(2)
+    homepage.check_products_count(2)
+    #monitors = browser.find_elements(By.CSS_SELECTOR, value = '.card')
+    #assert len(monitors) == 2
